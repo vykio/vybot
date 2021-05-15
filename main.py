c47824760357736ad00ad05bc12c6dcba81c285e
Alexandre <alexandrevasseur06@gmail.com>
2021-05-15 18:41:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import discord
 import re
-from keep_alive import keep_alive
 import asyncio
 import requests
 
@@ -57,5 +56,4 @@ async def on_message(message):
 
 bot_token = os.environ['bot_token']
 
-keep_alive()
 client.run(bot_token)
